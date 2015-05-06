0de3acdfec1a56cc203bb1bd0c8632bb9b3327e9
Rich Harris <richard.a.harris@gmail.com>
2015-05-06 19:37:15 -0400

    various fixes and improvements

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+var path = require( 'path' );
+var fs = require( 'fs' );
 var tosource = require( 'tosource' );
+var _spelunk = require( 'spelunk' );
 
-module.exports = function ( inputDir, outputDir, options, done ) {
-	require( 'spelunk' )( inputDir, function ( err, result ) {
-		var stringify = stringifiers[ options.type || 'json' ];
+module.exports = function spelunk ( inputdir, outputdir, options, callback ) {
+	var stringify = stringifiers[ options.type || 'json' ];
 
-		if ( !options.dest ) {
-			err = 'You must specify a `dest` property for the gobble-spelunk transform';
-		}
+	if ( !options.dest ) {
+		throw new Error( 'You must specify a `dest` property for the gobble-spelunk transform' );
+	}
 
-		if ( !stringify ) {
-			err = 'You must specify a `type` property for the gobble-spelunk plugin. Supported types are ' + Object.keys( stringifiers );
-		}
+	if ( !stringify ) {
+		throw new Error( 'You must specify a `type` property for the gobble-spelunk plugin. Supported types are ' + Object.keys( stringifiers ) );
+	}
 
-		if ( err ) return done( err );
+	if ( options.spelunk ) {
+		console.log( 'options.spelunk is deprecated - `exclude` and other options are passed straight through to spelunk' );
+	}
 
-		require( 'gobble' ).file.write( outputDir, options.dest, stringify( result, options ) ).then( done );
+	_spelunk( inputdir, options.spelunk || options ).then( function ( result ) {
+		fs.writeFile( path.join( outputdir, options.dest ), stringify( result, options ), callback );
 	});
 };
 
@@ -23,15 +28,15 @@ var stringifiers = {
 		return JSON.stringify( result, options.replacer, options.space );
 	},
 
-	amd: function ( result, options ) {
+	amd: function ( result ) {
 		return 'define(function(){return ' + tosource( result ) + ';});';
 	},
 
-	cjs: function ( result, options ) {
+	cjs: function ( result ) {
 		return 'module.exports = ' + tosource( result ) + ';';
 	},
 
-	es6: function ( result, options ) {
+	es6: function ( result ) {
 		return 'export default (' + tosource( result ) + ');';
 	}
-}
+};
